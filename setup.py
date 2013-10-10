807140f687c9d602374048c0a94516aa3bf03e24
Philippe Tartaise <ptartaise@dynamikdev.com>
2013-10-10 11:34:42 +0200

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pyDictbyDatetime',
+    version='0.1',
+    packages=['pydictdatetime'],
+    url='https://github.com/dynamikdev/pyDictbyDatetime',
+    license='GPLV3',
+    author='philippe',
+    author_email='ptartaise@dynamikdev.com',
+    description="""a python package that's provide a dictionary with values depens on a datetime"""
+)
